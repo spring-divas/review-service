56b25a776a46185069039feac6e4dba5e093106f
katebratiuk <kate.bratiuk@gmail.com>
2026-09-17 20:51:22 +0300

    chore: set up empty gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+	java
+	checkstyle
+	pmd
+	jacoco
+	id("org.springframework.boot") version "4.1.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.spring.divas"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testCompileOnly("org.projectlombok:lombok")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testAnnotationProcessor("org.projectlombok:lombok")
+}
+
+checkstyle {
+	toolVersion = "13.7.0"
+	configFile = file("checks.xml")
+}
+
+pmd {
+	isConsoleOutput = true
+}
+
+jacoco {
+	toolVersion = "0.8.15"
+}
+
+tasks.test {
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(true)
+		csv.required.set(true)
+		html.required.set(true)
+	}
+}
