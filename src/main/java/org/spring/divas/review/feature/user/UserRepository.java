3f281bbfa65ec87cc13f775c2d5a841b20dbbc99
katebratiuk <kate.bratiuk@gmail.com>
2026-09-19 18:55:45 +0300

    fix(#3): add repos

---
+++ src/main/java/org/spring/divas/review/feature/user/UserRepository.java
@@ -0,0 +1,7 @@
+package org.spring.divas.review.feature.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
