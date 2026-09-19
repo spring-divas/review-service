7d02623d71f49cd8073c11713fc32ecd6dde2e88
katebratiuk <kate.bratiuk@gmail.com>
2026-09-19 17:38:01 +0300

    feat(#1): add user entity

---
+++ src/main/java/org/spring/divas/review/feature/user/User.java
@@ -0,0 +1,28 @@
+package org.spring.divas.review.feature.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+  @Id
+  private Long id;
+
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
+
+  @Column(name = "last_name", nullable = false)
+  private String lastName;
+}
