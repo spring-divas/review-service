c4cd34a86ae8c9d238d4c68dca7951fdbc74e387
katebratiuk <kate.bratiuk@gmail.com>
2026-09-19 17:07:44 +0300

    ci: reuse workflow from common, update .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
 	testCompileOnly("org.projectlombok:lombok")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testAnnotationProcessor("org.projectlombok:lombok")
+	testImplementation("com.h2database:h2")
 }
 
 checkstyle {
