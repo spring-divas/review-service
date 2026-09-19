b65aad4c66c4c6bebc54dee0a52f4c5e310d8625
katebratiuk <kate.bratiuk@gmail.com>
2026-09-19 17:37:50 +0300

    feat(#1): add VenueReview

---
+++ src/main/java/org/spring/divas/review/feature/venue/VenueReview.java
@@ -0,0 +1,42 @@
+package org.spring.divas.review.feature.venue;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.spring.divas.review.common.entity.BaseEntity;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@Entity
+@Table(
+    name = "venue_reviews",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "venue_id"})
+)
+public class VenueReview extends BaseEntity {
+
+  @Column(name = "user_id", nullable = false)
+  private Long userId;
+
+  @Column(name = "content", columnDefinition = "TEXT")
+  private String content;
+
+  @Column(name = "rating", nullable = false)
+  private Short rating;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDate createdAt;
+
+  @Column(name = "venue_id", nullable = false)
+  private Long venueId;
+
+}
