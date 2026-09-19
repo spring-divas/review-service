90bc73d281c9212beb5c584608f3986881024d79
katebratiuk <kate.bratiuk@gmail.com>
2026-09-19 18:09:32 +0300

    fix(#1): validate user's name

--- src/main/java/org/spring/divas/review/feature/user/User.java
+++ src/main/java/org/spring/divas/review/feature/user/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,9 +22,13 @@ public class User {
   @Id
   private Long id;
 
-  @Column(name = "first_name", nullable = false)
+  @NotBlank
+  @Size(max = 100)
+  @Column(name = "first_name", nullable = false, length = 100)
   private String firstName;
 
-  @Column(name = "last_name", nullable = false)
+  @NotBlank
+  @Size(max = 100)
+  @Column(name = "last_name", nullable = false, length = 100)
   private String lastName;
 }
